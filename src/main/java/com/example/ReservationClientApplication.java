242816264c0fa8a5247f724510391f1c11959eee
samzhu <samzhu@mitake.com.tw>
2016-01-08 19:46:01 +0800

    fix

--- src/main/java/com/example/ReservationClientApplication.java
+++ src/main/java/com/example/ReservationClientApplication.java
@@ -1,27 +1,33 @@
 package com.example;
 
+import java.net.URI;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.stream.Collectors;
 
+import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 //import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
-import org.springframework.cloud.stream.annotation.EnableBinding;
-import org.springframework.cloud.stream.annotation.Output;
-import org.springframework.cloud.stream.messaging.Source;
+//import org.springframework.cloud.stream.annotation.EnableBinding;
+//import org.springframework.cloud.stream.annotation.Output;
+//import org.springframework.cloud.stream.messaging.Source;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.hateoas.Resources;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
-import org.springframework.messaging.MessageChannel;
-import org.springframework.messaging.support.MessageBuilder;
+//import org.springframework.messaging.MessageChannel;
+//import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -30,17 +36,24 @@ import org.springframework.web.client.RestTemplate;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
-@EnableZuulProxy
+//@EnableZuulProxy
 //@EnableBinding(Source.class)
 @EnableCircuitBreaker
 @EnableDiscoveryClient
 @SpringBootApplication
 public class ReservationClientApplication {
 	
+	
 //	@Bean
 //	AlwaysSampler alwaysSampler(){
 //		return new AlwaysSampler();
 //	}
+	
+	@Bean
+    CommandLineRunner dc(DiscoveryClient dc) {
+		return args -> dc.getInstances("reservation-service")
+                .forEach(si -> System.out.println(String.format("%s %s:%s", si.getServiceId(), si.getHost(), si.getPort())));
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReservationClientApplication.class, args);
@@ -54,6 +67,12 @@ class ReservationApiGatewayRestController{
 	@Autowired
 	@LoadBalanced
 	private RestTemplate restTemplate;
+	
+	@Autowired
+	private LoadBalancerClient loadBalancer;
+	
+	// @Autowired
+	 //private DiscoveryClient discoveryClient;
 	
 //	@Autowired
 //	@Output(Source.OUTPUT)
@@ -71,12 +90,18 @@ class ReservationApiGatewayRestController{
 	@HystrixCommand(fallbackMethod = "getReservationNamesFallback")
 	@RequestMapping("names")
     public Collection<String> getReservationNames(){
+		
+		ServiceInstance instance = loadBalancer.choose("reservation-service");
+		//instance.getUri().get
+		URI serviceUri = URI.create(String.format("http://%s:%s", instance.getHost(),instance.getPort()));
+		
+		System.out.println(String.format("http://%s:%s", instance.getHost(),instance.getPort()));
 
         ParameterizedTypeReference<Resources<Reservation>> ptr = 
                 new ParameterizedTypeReference<Resources<Reservation>>(){};
-
+                
                 ResponseEntity<Resources<Reservation>> responseEntity = 
-                        this.restTemplate.exchange("http://reservation-service/reservations", 
+                		this.restTemplate.exchange("http://reservation-service/reservations", 
                                 HttpMethod.GET, null, ptr);
                 
                 Collection<String> nameList = responseEntity
@@ -87,6 +112,22 @@ class ReservationApiGatewayRestController{
                         .collect(Collectors.toList());
 
                 return nameList;
+
+    }
+	
+	//@HystrixCommand(fallbackMethod = "getReservationNamesFallback")
+	@RequestMapping("message")
+    public String getmessage(){
+
+        ParameterizedTypeReference<Resources<Reservation>> ptr = 
+                new ParameterizedTypeReference<Resources<Reservation>>(){};
+                
+                ResponseEntity<Resources<Reservation>> responseEntity = 
+                		this.restTemplate.exchange("http://reservation-service/message", 
+                                HttpMethod.GET, null, ptr);
+
+                return responseEntity.getBody().toString();
+
     }
 	
 //	@HystrixCommand(fallbackMethod = "getReservationNamesFallback")
